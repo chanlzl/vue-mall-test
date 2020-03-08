60c54bd9a58e4ab4e4122fd1555279b2f762e369
chanlzl <1342143957@qq.com>
2020-03-08 18:21:48 +0800

    vue-mall-test

---
+++ src/components/GoodList.vue
@@ -0,0 +1,83 @@
+<template>
+    <div name="show">
+        <ul class="goodlist">
+            <li v-for="goods in list">
+                <img v-bind:src="goods.img">
+                <p>{{goods.goodName}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<style>
+    .goodlist li
+    {
+        width: 200px;
+        height: 200px;
+        list-style: none;
+        float: left;
+        font-size: 12px;
+        color: red;
+        margin-bottom: 30px;
+    }
+    .goodlist img
+    {
+        width: 180px;
+        height: 180px;
+    }
+</style>
+<script>
+export default {
+    name:"show",
+    data(){
+        var obj=this;
+        var url="";
+        if(obj.goodId==1){
+            url="json/pc.json";
+        }
+        else if(obj.goodId==2)
+        {
+            url="json/phone.json";
+        }
+        else
+        {
+            url="json/pc.json";
+        }
+        this.$http.get(url).then(function(res){
+            obj.list=res.data;
+        })
+        return{
+            list:[]
+        }
+    },
+    props:
+    {
+        goodId:Number
+    },
+    watch:
+    {
+        goodId()
+        {
+            var obj=this;
+            var url="";
+            if(obj.goodId==1){
+                url="json/pc.json";
+            }
+            else if(obj.goodId==2)
+            {
+                url="json/phone.json";
+            }
+            else
+            {
+                url="json/pc.json";
+            }
+            this.$http.get(url).then(function(res){
+            obj.list=res.data;
+            })
+            return
+            {
+                list:[]
+            }
+        }
+    }
+}
+</script>
---
+++ src/components/Right.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div v-if="ll==1">
+            <GoodList :goodId="1"></GoodList>
+        </div>
+        <div v-else-if="ll==2">
+            <GoodList :goodId="2"></GoodList>
+        </div>
+        <div v-else-if="ll==3">
+            3333
+        </div>
+        <div v-else-if="ll==4">
+        </div>
+        <div v-else>
+            <GoodList></GoodList>
+        </div>
+    </div>
+</template>
+<script>
+import Msg from './Msg.js'
+import GoodList from './GoodList.vue'
+export default 
+{
+    data(){
+        return{
+            ll:0
+        }
+    },
+    mounted:function()
+        {
+            var _this=this
+            Msg.$on('val',function(m)
+                {
+                    _this.ll=m;
+                })
+        },
+        components:{
+            GoodList
+        }
+}
+</script>
